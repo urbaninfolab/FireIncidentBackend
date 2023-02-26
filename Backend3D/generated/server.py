75d7172272806436d3c0bfcd9627331bf099743b
ryanhlewis <rhl@utexas.edu>
2023-02-25 19:03:34 -0600

    Create server.py

---
+++ Backend3D/generated/server.py
@@ -0,0 +1,13 @@
+# Simple Python server to host files for the web
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
